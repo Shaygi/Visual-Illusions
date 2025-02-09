1de1efbdb945838bbac036f59bad1217e1bdfd44
Shaygi <serol@stud.hs-bremen.de>
2025-02-09 16:21:50 +0100

    Added non euclidean Portal

---
+++ VisualIllusions/Assets/Scripts/PortalCameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PortalCameraController : MonoBehaviour
+{
+    [Header("Portal-Referenzen")]
+    [Tooltip("Das Portal, vor dem der Spieler steht (Eingangsportal).")]
+    public Transform entrancePortal;
+
+    [Tooltip("Das gegenüberliegende Portal (Ausgangsportal).")]
+    public Transform exitPortal;
+
+    [Tooltip("Die Spieler-Kamera (z. B. Main Camera).")]
+    public Transform playerCamera;
+
+    void LateUpdate()
+    {
+        // Berechne die Position des Spielers relativ zum Eingang-Portal
+        Vector3 relativePos = entrancePortal.InverseTransformPoint(playerCamera.position);
+
+        // Transformiere die relative Position in den Raum des Ausgang-Portals
+        transform.position = exitPortal.TransformPoint(relativePos);
+
+        // Berechne die relative Rotation
+        Quaternion relativeRot = Quaternion.Inverse(entrancePortal.rotation) * playerCamera.rotation;
+        transform.rotation = exitPortal.rotation * relativeRot;
+    }
+}
